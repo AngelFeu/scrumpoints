a723377640cfec43f7cac1d03d21fa64427a6d18
feu.angel <angel.feu.ext@level3.com>
2020-06-01 11:56:04 -0300

    cambio a server local

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: `https://9f554089.ngrok.io/api/`
+    baseURL: `https://b2249214f3b7.ngrok.io/api/`
     //baseURL: `http://localhost:4000/api/`
 });
