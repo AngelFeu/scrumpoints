3f84907b332b3b6761d46d4a05e72c74e2e3da68
feu.angel <angel.feu.ext@level3.com>
2020-06-01 20:28:47 -0300

    prueba ngrok

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: `https://b2249214f3b7.ngrok.io/api/`
+    baseURL: `https://4519f263185f.ngrok.io/api/`
     //baseURL: `http://localhost:4000/api/`
 });
